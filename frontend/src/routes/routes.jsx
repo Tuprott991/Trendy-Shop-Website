c74e2747565fa97559b6878efbd79bc961ae60a9
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2024-12-05 17:11:22 +0700

    Update routes for admin and retailer role in frontend

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -4,68 +4,72 @@ import Signup from "../pages/authentication/Signup.jsx";
 import CustomerLayout from "../pages/customer/CustomerLayout.jsx";
 import CustomerHome from "../pages/customer/CustomerHome.jsx";
 import CustomerProductDetail from "../pages/customer/CustomerProductDetail.jsx";
+import RetailerLayout from "../pages/retailer/RetailerLayout.jsx";
+import AdminLayout from "../pages/admin/AdminLayout.jsx";
 import Authorization from "../auth/Authorization.jsx";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext.jsx";
 
 const AppRoutes = () => {
- const { isAuthenticated } = useContext(AuthContext);
- const role = localStorage.getItem("role");
+    const { isAuthenticated } = useContext(AuthContext);
+    const role = localStorage.getItem("role");
 
- const routes = useRoutes([
-  {
-   path: "login",
-   element: isAuthenticated ? <Navigate to={`/${role}`} replace /> : <Login />,
-  },
-  {
-   path: "signup",
-   element: isAuthenticated ? <Navigate to={`/${role}`} replace /> : <Signup />,
-  },
+    const routes = useRoutes([
+        {
+            path: "login",
+            element: isAuthenticated ? <Navigate to={`/${role}`} replace /> : <Login />,
+        },
+        {
+            path: "signup",
+            element: isAuthenticated ? <Navigate to={`/${role}`} replace /> : <Signup />,
+        },
 
-  {
-   element: <Authorization allowedRoles={["customer"]} />,
-   children: [
-    {
-     path: "customer",
-     element: <CustomerLayout />,
-     children: [
-      {
-       index: true, // Default route for `/customer`
-       element: <CustomerHome />,
-      },
-      {
-       path: "product/:id", // Route for `/customer/product/:id`
-       element: <CustomerProductDetail />,
-      },
-     ],
-    },
-   ],
-  },
+        {
+            element: <Authorization allowedRoles={["customer"]} />,
+            children: [
+                {
+                    path: "customer",
+                    element: <CustomerLayout />,
+                    children: [
+                        {
+                            index: true, // Default route for `/customer`
+                            element: <CustomerHome />,
+                        },
+                        {
+                            path: "product/:id", // Route for `/customer/product/:id`
+                            element: <CustomerProductDetail />,
+                        },
+                    ],
+                },
+            ],
+        },
 
-  // Retailer Routes
-  {
-   element: <Authorization allowedRoles={["retailer"]} />,
-   children: [
-    {
-     path: "retailer",
-     //  element: <RetailerLayout />,
-    },
-   ],
-  },
+        // Retailer Routes
+        {
+            element: <Authorization allowedRoles={["retailer"]} />,
+            children: [
+                {
+                    path: "retailer",
+                    index: true,
+                    element: <RetailerLayout />,
+                },
+            ],
+        },
 
-  // Admin Routes
-  {
-   element: <Authorization allowedRoles={["admin"]} />,
-   children: [
-    {
-     path: "admin",
-     //  element: <AdminLayout />,
-    },
-   ],
-  },
- ]);
+        // Admin Routes
+        {
+            element: <Authorization allowedRoles={["admin"]} />,
+            children: [
+                {
+                    path: "admin",
+                    element: <AdminLayout />,
+                    index: true,
+                },
+            ],
+        },
+    ]);
 
- return routes;
+    return routes;
 };
 
 export default AppRoutes;
