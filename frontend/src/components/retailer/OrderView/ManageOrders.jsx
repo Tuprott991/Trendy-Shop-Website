931dd349bd5f2a59d5fac821be05dae3f21101fe
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2024-12-05 10:08:08 +0700

    Update source for retailer frontend: addition new component <Pagination>

--- frontend/src/components/retailer/OrderView/ManageOrders.jsx
+++ frontend/src/components/retailer/OrderView/ManageOrders.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FaEye, FaPen, FaTrash } from "react-icons/fa";
 import { BsPatchExclamation } from "react-icons/bs";
-import ReactPaginate from "react-paginate";
+import Pagination from "../Helper/pagination.jsx";
 
 const OrdersTable = () => {
     const [orders, setOrders] = useState([
@@ -15,16 +15,6 @@ const OrdersTable = () => {
         { id: "08", name: "Tu Nguyen", email: "tunguyen@gmail.com", address: "123 Nguyen Van Cu", phone: "0987776782", status: "Pending" },
         { id: "09", name: "Tu Nguyen", email: "tunguyen@gmail.com", address: "123 Nguyen Van Cu", phone: "0987776782", status: "Pending" },
     ]);
-
-    const [currentPage, setCurrentPage] = useState(0);
-    const itemsPerPage = 5;
-    const offset = currentPage * itemsPerPage;
-    const currentItems = orders.slice(offset, offset + itemsPerPage);
-    const pageCount = Math.ceil(orders.length / itemsPerPage);
-
-    const handlePageClick = ({ selected }) => {
-        setCurrentPage(selected);
-    };
 
     const handleStatusChange = (id, newStatus) => {
         setOrders((prevOrders) =>
@@ -63,84 +53,73 @@ const OrdersTable = () => {
         setSelectedVoucher(null);
     };
 
+    const renderItems = (currentItems) => (
+        <div className="overflow-hidden rounded-xl shadow-lg bg-white">
+            <table className="min-w-full table-auto">
+                <thead className="bg-gradient-to-r from-green-500 to-blue-500 text-white">
+                    <tr>
+                        <th className="px-6 py-3 text-center font-semibold">ID</th>
+                        <th className="px-6 py-3 text-center font-semibold">Name</th>
+                        <th className="px-6 py-3 text-center font-semibold">Email</th>
+                        <th className="px-6 py-3 text-center font-semibold">Address</th>
+                        <th className="px-6 py-3 text-center font-semibold">Phone</th>
+                        <th className="px-6 py-3 text-center font-semibold">Status</th>
+                        <th className="px-6 py-3 text-center font-semibold">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {currentItems.map((order, index) => (
+                        <tr key={index} className="border-b hover:bg-gray-100 transition duration-300 ease-in-out">
+                            <td className="px-6 py-4 text-center">{order.id}</td>
+                            <td className="px-6 py-4 text-center">{order.name}</td>
+                            <td className="px-6 py-4 text-center">{order.email}</td>
+                            <td className="px-6 py-4 text-center">{order.address}</td>
+                            <td className="px-6 py-4 text-center">{order.phone}</td>
+                            <td className="px-6 py-4 text-center">
+                                <select
+                                    value={order.status}
+                                    onChange={(e) => handleStatusChange(order.id, e.target.value)}
+                                    className={`rounded px-3 py-1.5 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 ${order.status === "Pending"
+                                        ? "bg-gray-200 text-black"
+                                        : "bg-amber-200 text-black"
+                                        }`}
+                                >
+                                    <option value="Pending" className="text-center">Pending</option>
+                                    <option value="Completed" className="text-center">Completed</option>
+                                </select>
+                            </td>
+                            <td className="px-6 py-4 text-center">
+                                <div className="flex space-x-4 justify-center">
+                                    <button className="hover:text-blue-500 transition-all" onClick={handleViewClick}>
+                                        <FaEye size={20} />
+                                    </button>
+
+                                    <button className="hover:text-blue-500 transition-all" onClick={handleEditClick}>
+                                        <FaPen size={20} />
+                                    </button>
+
+                                    <button
+                                        className="hover:text-red-500 transition-all"
+                                        onClick={() => handleDeleteClick(order.id)}
+                                    >
+                                        <FaTrash size={20} />
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+
     return (
         <div className="px-6 py-5">
-            <div className="overflow-hidden rounded-xl shadow-lg bg-white">
-                <table className="min-w-full table-auto">
-                    <caption className="text-lg font-bold pb-4 pt-4 bg-gradient-to-r from-green-400 to-blue-500 text-white">
-                        <div className="flex justify-between items-center pl-6 pr-3">
-                            <p className="text-xl">Orders List</p>
-                        </div>
-                    </caption>
-                    <thead className="bg-gradient-to-r from-green-500 to-blue-500 text-white">
-                        <tr>
-                            <th className="px-6 py-3 text-center font-semibold">ID</th>
-                            <th className="px-6 py-3 text-center font-semibold">Name</th>
-                            <th className="px-6 py-3 text-center font-semibold">Email</th>
-                            <th className="px-6 py-3 text-center font-semibold">Address</th>
-                            <th className="px-6 py-3 text-center font-semibold">Phone</th>
-                            <th className="px-6 py-3 text-center font-semibold">Status</th>
-                            <th className="px-6 py-3 text-center font-semibold">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {currentItems.map((order, index) => (
-                            <tr key={index} className="border-b hover:bg-gray-100 transition duration-300 ease-in-out">
-                                <td className="px-6 py-4 text-center">{order.id}</td>
-                                <td className="px-6 py-4 text-center">{order.name}</td>
-                                <td className="px-6 py-4 text-center">{order.email}</td>
-                                <td className="px-6 py-4 text-center">{order.address}</td>
-                                <td className="px-6 py-4 text-center">{order.phone}</td>
-                                <td className="px-6 py-4 text-center">
-                                    <select
-                                        value={order.status}
-                                        onChange={(e) => handleStatusChange(order.id, e.target.value)}
-                                        className={`rounded px-3 py-1.5 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 ${order.status === "Pending"
-                                            ? "bg-gray-200 text-black"
-                                            : "bg-amber-200 text-black"
-                                            }`}
-                                    >
-                                        <option value="Pending" className="text-center">Pending</option>
-                                        <option value="Completed" className="text-center">Completed</option>
-                                    </select>
-                                </td>
-                                <td className="px-6 py-4 text-center">
-                                    <div className="flex space-x-4 justify-center">
-                                        <button className="hover:text-blue-500 transition-all" onClick={handleViewClick}>
-                                            <FaEye size={20} />
-                                        </button>
-
-                                        <button className="hover:text-blue-500 transition-all" onClick={handleEditClick}>
-                                            <FaPen size={20} />
-                                        </button>
-
-                                        <button
-                                            className="hover:text-red-500 transition-all"
-                                            onClick={() => handleDeleteClick(order.id)}
-                                        >
-                                            <FaTrash size={20} />
-                                        </button>
-                                    </div>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-            <div className="flex justify-center items-center mt-5">
-                <ReactPaginate
-                    previousLabel={"Previous"}
-                    nextLabel={"Next"}
-                    pageCount={pageCount}
-                    onPageChange={handlePageClick}
-                    containerClassName={"flex space-x-2"}
-                    previousClassName={`px-4 py-2 rounded-lg ${currentPage === 0 ? "cursor-not-allowed text-gray-500" : "bg-blue-500 text-white hover:bg-blue-600"}`}
-                    nextClassName={`px-4 py-2 rounded-lg ${currentPage === pageCount - 1 ? "cursor-not-allowed text-gray-500" : "bg-blue-500 text-white hover:bg-blue-600"}`}
-                    activeClassName={"px-4 py-2 bg-gray-300 text-white rounded"}
-                    pageClassName={"px-4 py-2 rounded text-neutral-400 font-bold"}
-                    disabledClassName={"cursor-not-allowed"}
-                />
-            </div>
+            <Pagination
+                data={orders}
+                itemsPerPage={5}
+                renderItems={renderItems}
+            />
             {isModalOpen && (
                 <div className="fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50 z-50">
                     <div className="bg-white rounded-lg shadow-lg p-6">
--- frontend/src/components/retailer/VoucherView/ManageVouchers.jsx
+++ frontend/src/components/retailer/VoucherView/ManageVouchers.jsx
@@ -1,35 +1,27 @@
 import React, { useState } from "react";
 import { FaEye, FaPen, FaTrash } from "react-icons/fa";
 import { BsPatchExclamation } from "react-icons/bs";
-import ReactPaginate from "react-paginate";
+import Pagination from "../Helper/pagination.jsx";
 
-const VoucherList = () => {
-    const [vouchers, setVouchers] = useState([
-        { id: "01", name: "voucher01", description: "voucher01", discount: 50, upToDate: "30 Nov 2024", status: "Incomplete" },
-        { id: "02", name: "voucher02", description: "voucher02", discount: 10, upToDate: "30 Nov 2024", status: "Complete" },
-        { id: "03", name: "voucher03", description: "voucher03", discount: 20, upToDate: "30 Nov 2024", status: "Incomplete" },
+const OrdersTable = () => {
+    const [orders, setOrders] = useState([
+        { id: "01", name: "Tu", email: "tu@gmail.com", address: "123 Nguyen Van Cu", phone: "0987776789", status: "Inactived" },
+        { id: "02", name: "Tu", email: "tu@gmail.com", address: "123 Nguyen Van Cu", phone: "0987776789", status: "Inactived" },
+        { id: "03", name: "Tu", email: "tu@gmail.com", address: "123 Nguyen Van Cu", phone: "0987776789", status: "Inactived" },
+        { id: "04", name: "Tu", email: "tu@gmail.com", address: "123 Nguyen Van Cu", phone: "0987776789", status: "Inactived" },
+        { id: "05", name: "Tu", email: "tu@gmail.com", address: "123 Nguyen Van Cu", phone: "0987776789", status: "Inactived" },
+        { id: "06", name: "Tu", email: "tu@gmail.com", address: "123 Nguyen Van Cu", phone: "0987776789", status: "Inactived" },
+        { id: "07", name: "Tu", email: "tu@gmail.com", address: "123 Nguyen Van Cu", phone: "0987776789", status: "Inactived" },
+        { id: "08", name: "Tu", email: "tu@gmail.com", address: "123 Nguyen Van Cu", phone: "0987776789", status: "Inactived" },
+
     ]);
 
-    const [currentPage, setCurrentPage] = useState(0);
-    const itemsPerPage = 5;
-    const offset = currentPage * itemsPerPage;
-    const currentItems = vouchers.slice(offset, offset + itemsPerPage);
-    const pageCount = Math.ceil(vouchers.length / itemsPerPage);
-
-    const handlePageClick = ({ selected }) => {
-        setCurrentPage(selected);
-    };
-
     const handleStatusChange = (id, newStatus) => {
-        setVouchers((prevVouchers) =>
-            prevVouchers.map((voucher) =>
-                voucher.id === id ? { ...voucher, status: newStatus } : voucher
+        setOrders((prevOrders) =>
+            prevOrders.map((order) =>
+                order.id === id ? { ...order, status: newStatus } : order
             )
         );
-    };
-
-    const handleAddClick = () => {
-        console.log("Add clicked");
     };
 
     const handleViewClick = () => {
@@ -41,117 +33,98 @@ const VoucherList = () => {
     };
 
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedVoucher, setSelectedVoucher] = useState(null);
+    const [selectedOrderId, setSelectedOrderId] = useState(null);
 
     const handleDeleteClick = (id) => {
-        setSelectedVoucher(id);
+        setSelectedOrderId(id);
         setIsModalOpen(true);
     };
 
     const handleConfirmDelete = () => {
-        setVouchers((prevVouchers) =>
-            prevVouchers.filter((voucher) => voucher.id !== selectedVoucher)
+        setOrders((prevOrders) =>
+            prevOrders.filter((order) => order.id !== selectedOrderId)
         );
         setIsModalOpen(false);
-        setSelectedVoucher(null);
+        setSelectedOrderId(null);
     };
 
     const handleCloseModal = () => {
         setIsModalOpen(false);
-        setSelectedVoucher(null);
+        setSelectedOrderId(null);
     };
+
+    const renderItems = (currentItems) => (
+        <div className="overflow-hidden rounded-xl shadow-lg bg-white">
+            <table className="min-w-full table-auto">
+                <thead className="bg-gradient-to-r from-green-500 to-blue-500 text-white">
+                    <tr>
+                        <th className="px-6 py-3 text-center font-semibold">ID</th>
+                        <th className="px-6 py-3 text-center font-semibold">Name</th>
+                        <th className="px-6 py-3 text-center font-semibold">Email</th>
+                        <th className="px-6 py-3 text-center font-semibold">Address</th>
+                        <th className="px-6 py-3 text-center font-semibold">Phone</th>
+                        <th className="px-6 py-3 text-center font-semibold">Status</th>
+                        <th className="px-6 py-3 text-center font-semibold">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {currentItems.map((order) => (
+                        <tr key={order.id} className="border-b hover:bg-gray-100 transition duration-300 ease-in-out">
+                            <td className="px-6 py-4 text-center">{order.id}</td>
+                            <td className="px-6 py-4 text-center">{order.name}</td>
+                            <td className="px-6 py-4 text-center">{order.email}</td>
+                            <td className="px-6 py-4 text-center">{order.address}</td>
+                            <td className="px-6 py-4 text-center">{order.phone}</td>
+                            <td className="px-6 py-4 text-center">
+                                <select
+                                    value={order.status}
+                                    onChange={(e) => handleStatusChange(order.id, e.target.value)}
+                                    className={`rounded px-3 py-1.5 text-sm ${order.status === "Inactived"
+                                        ? "bg-gray-200 text-black"
+                                        : "bg-amber-200 text-black"
+                                    }`}
+                                >
+                                    <option value="Inactived">Inactived</option>
+                                    <option value="Completed">Actived</option>
+                                </select>
+                            </td>
+                            <td className="px-6 py-4 text-center">
+                                <div className="flex space-x-4 justify-center">
+                                    <button className="hover:text-gray-800 transition-all" onClick={handleViewClick}>
+                                        <FaEye size={20} />
+                                    </button>
+                                    <button className="hover:text-gray-800 transition-all" onClick={handleEditClick}>
+                                        <FaPen size={20} />
+                                    </button>
+                                    <button
+                                        className="hover:text-red-500 transition-all"
+                                        onClick={() => handleDeleteClick(order.id)}
+                                    >
+                                        <FaTrash size={20} />
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 
     return (
         <div className="px-6 py-5">
-            <div className="overflow-hidden rounded-xl shadow-lg bg-white">
-                <table className="min-w-full table-auto">
-                    <caption className="text-lg font-bold pb-4 pt-4 bg-gradient-to-r from-green-400 to-blue-500 text-white">
-                        <div className="flex justify-between items-center pl-6 pr-3">
-                            <p className="text-xl">Voucher List</p>
-                            <button
-                                onClick={handleAddClick}
-                                className="bg-green-500 text-white py-2 px-5 text-sm rounded-lg shadow-lg hover:bg-green-600 transition duration-300"
-                            >
-                                Add Voucher
-                            </button>
-                        </div>
-                    </caption>
-                    <thead className="bg-gradient-to-r from-green-500 to-blue-500 text-white">
-                        <tr>
-                            <th className="px-6 py-3 text-center font-semibold">ID</th>
-                            <th className="px-6 py-3 text-center font-semibold">Name</th>
-                            <th className="px-6 py-3 text-center font-semibold">Description</th>
-                            <th className="px-6 py-3 text-center font-semibold">Discount (%)</th>
-                            <th className="px-6 py-3 text-center font-semibold">Up-to-date</th>
-                            <th className="px-6 py-3 text-center font-semibold">Status</th>
-                            <th className="px-6 py-3 text-center font-semibold">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {currentItems.map((voucher) => (
-                            <tr key={voucher.id} className="border-b hover:bg-gray-100 transition duration-300 ease-in-out">
-                                <td className="px-6 py-4 text-center">{voucher.id}</td>
-                                <td className="px-6 py-4 text-center">{voucher.name}</td>
-                                <td className="px-6 py-4 text-center">{voucher.description}</td>
-                                <td className="px-6 py-4 text-center">{voucher.discount}</td>
-                                <td className="px-6 py-4 text-center">{voucher.upToDate}</td>
-                                <td className="px-6 py-4 text-center">
-                                    <select
-                                        value={voucher.status}
-                                        onChange={(e) => handleStatusChange(voucher.id, e.target.value)}
-                                        className={`rounded px-3 py-1.5 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 ${voucher.status === "Complete"
-                                            ? "bg-green-200 text-black"
-                                            : "bg-yellow-200 text-black"
-                                            }`}
-                                    >
-                                        <option value="Complete">Complete</option>
-                                        <option value="Incomplete">Incomplete</option>
-                                    </select>
-                                </td>
-                                <td className="px-6 py-4 text-center">
-                                    <div className="flex space-x-4 justify-center">
-                                        <button className="hover:text-blue-500 transition-all" onClick={handleViewClick}>
-                                            <FaEye size={20} />
-                                        </button>
-                                        <button className="hover:text-blue-500 transition-all" onClick={handleEditClick}>
-                                            <FaPen size={20} />
-                                        </button>
-                                        <button
-                                            className="hover:text-red-500 transition-all"
-                                            onClick={() => handleDeleteClick(voucher.id)}
-                                        >
-                                            <FaTrash size={20} />
-                                        </button>
-                                    </div>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-
-            <div className="flex justify-center items-center mt-5">
-                <ReactPaginate
-                    previousLabel={"Previous"}
-                    nextLabel={"Next"}
-                    pageCount={pageCount}
-                    onPageChange={handlePageClick}
-                    containerClassName={"flex space-x-2"}
-                    previousClassName={`px-4 py-2 rounded-lg ${currentPage === 0 ? "cursor-not-allowed text-gray-500" : "bg-blue-500 text-white hover:bg-blue-600"}`}
-                    nextClassName={`px-4 py-2 rounded-lg ${currentPage === pageCount - 1 ? "cursor-not-allowed text-gray-500" : "bg-blue-500 text-white hover:bg-blue-600"}`}
-                    activeClassName={"px-4 py-2 bg-gray-300 text-white rounded"}
-                    pageClassName={"px-4 py-2 rounded text-neutral-400 font-bold"}
-                    disabledClassName={"cursor-not-allowed"}
-                />
-            </div>
-
+            <Pagination
+                data={orders}
+                itemsPerPage={5}
+                renderItems={renderItems}
+            />
             {isModalOpen && (
                 <div className="fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50 z-50">
                     <div className="bg-white rounded-lg shadow-lg p-6">
                         <div className="flex justify-center items-center mb-4">
                             <BsPatchExclamation size={150} />
                         </div>
-                        <p className="text-lg font-semibold mb-4">Are you sure want to delete this voucher?</p>
+                        <p className="text-lg font-semibold mb-4">Are you sure you want to delete this order?</p>
                         <div className="flex justify-end space-x-4">
                             <button
                                 className="bg-gray-300 text-black px-4 py-2 rounded font-bold"
@@ -173,4 +146,4 @@ const VoucherList = () => {
     );
 };
 
-export default VoucherList;
+export default OrdersTable;
