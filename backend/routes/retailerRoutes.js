fd1e286534ef0f83f9b3496a9e556c76b6bb656c
NghiaZun <dtnghia22@clc.fitus.edu.vn>
2024-12-09 10:58:06 +0700

    Update add product page

--- backend/routes/retailerRoutes.js
+++ backend/routes/retailerRoutes.js
@@ -4,10 +4,12 @@ const express = require("express");
 module.exports = () => {
   const voucherPage = require("../controllers/VoucherController/voucher.Controller");
   const retailerDashboard = require("../controllers/UserController/user.controller");
+  const productPage = require("../controllers/ProductController/product");
   const router = express.Router();
 
   // Retailer Dashboard Routes
   router.get("/dashboard", retailerDashboard.getRetailerDashboardData);
+  router.post("/addproduct", productPage.importProduct);
   router.post("/addvoucher", voucherPage.postCreateVoucher);  
   router.get("/voucherpage", voucherPage.getVoucherPage);
 
