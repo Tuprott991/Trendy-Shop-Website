fbf25693609eff878a1686854e323f5a4500d396
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2024-12-28 15:30:51 +0700

    Fix bug

--- backend/routes/retailerRoutes.js
+++ backend/routes/retailerRoutes.js
@@ -3,30 +3,23 @@ const express = require("express");
 const authenticateToken = require("../middleware");
 
 module.exports = () => {
- const voucherPage = require("../controllers/VoucherController/voucher.Controller");
- const retailerDashboard = require("../controllers/UserController/user.controller");
- const productPage = require("../controllers/ProductController/product");
- const orderPage = require("../controllers/OrderController/order.controller");
- const router = express.Router();
+    const voucherPage = require("../controllers/VoucherController/voucher.Controller");
+    const retailerDashboard = require("../controllers/UserController/user.controller");
+    const productPage = require("../controllers/ProductController/product");
+    const orderPage = require("../controllers/OrderController/order.controller");
+    const router = express.Router();
 
- // Retailer Dashboard Routes
- router.get(
-  "/dashboard",
-  authenticateToken,
-  retailerDashboard.getRetailerDashboardData
- );
- router.post("/addproduct", authenticateToken, productPage.importProduct);
- router.post("/deleteproduct", productPage.postDeleteProduct);
+    router.get(
+        "/dashboard",
+        authenticateToken,
+        retailerDashboard.getRetailerDashboardData
+    );
+    router.post("/addproduct", authenticateToken, productPage.importProduct);
+    router.post("/deleteproduct", productPage.postDeleteProduct);
+    router.post("/addvoucher", authenticateToken, voucherPage.postCreateVoucher);
+    router.get("/voucher", authenticateToken, voucherPage.getVoucherPage);
+    router.post("/deletevoucher", voucherPage.postDeleteVoucher);
+    router.post("/updatevoucher", voucherPage.postUpdateVoucher);
 
- router.post("/addvoucher", authenticateToken, voucherPage.postCreateVoucher);
- router.get("/voucher", authenticateToken, voucherPage.getVoucherPage);
- router.post("/deletevoucher", voucherPage.postDeleteVoucher);
-<<<<<<< HEAD
-=======
- router.post("/updatevoucher", voucherPage.postUpdateVoucher);
->>>>>>> 1300e54f1f44dd94ad4ef65631854e79e54d78ae
-
- // router.get("/order",authenticateToken,orderPage.getOrderPage);
-
- return router; // Return the router
+    return router; // Return the router
 };
